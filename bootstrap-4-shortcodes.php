398226567112daae03d82baa10e19631a91c05f5
mwdelaney <michael.delaney@gmail.com>
2016-09-10 22:11:54 -0400

    Media button

--- bootstrap-4-shortcodes.php
+++ bootstrap-4-shortcodes.php
@@ -39,12 +39,48 @@ class Boostrap4Shortcodes {
 		if (version_compare(PHP_VERSION, '5.3.0', '<')) {
 			 add_action( 'admin_notices', array( $this, 'php_version_notice') );
 		}
+		//add the button to the content editor, next to the media button on any admin page in the array below
+		if(in_array(basename($_SERVER['PHP_SELF']), array('post.php', 'page.php', 'page-new.php', 'post-new.php', 'widgets.php', 'admin-ajax.php'))) {
+				add_action('media_buttons', array( $this, 'add_bootstrap_button' ), 11 );
+				add_action( 'media_buttons', array( $this, 'bootstrap_shortcodes_help_styles' ) );
+		}
 		//Conditionally include tooltip functionality (see function for conditionals)
 		//add_action( 'the_post', array( $this, 'bootstrap_shortcodes_tooltip_script' ), 9999 );
 		//Conditionally include popupver functionality (see function for conditionals)
 		//add_action( 'the_post', array( $this, 'bootstrap_shortcodes_popover_script' ), 9999 );
 	}
 
+	/**
+	 * Create the media-button to pop up the shortcode documentation
+	 */
+	function add_bootstrap_button() {
+			//the id of the container I want to show in the popup
+			$popup_id = 'bootstrap-shortcodes-help';
+			//our popup's title
+			$title = 'Bootstrap Shortcodes Help';
+			//append the icon
+			printf(
+			'<a data-toggle="modal" data-target="#bootstrap-shortcodes-help" title="%2$s" href="%3$s" class="%4$s"><span class="bs_bootstrap-logo wp-media-buttons-icon"></span></a>',
+			esc_attr( $popup_id ),
+			esc_attr( $title ),
+			esc_url( '#' ),
+			esc_attr( 'button add_media bootstrap-shortcodes-button')
+			//sprintf( '<img src="%s" style="height: 20px; position: relative; top: -2px;">', esc_url( $img ) )
+			);
+	}
+
+
+	//Function to register and enqueue the documentation stylesheets
+	function bootstrap_shortcodes_help_styles() {
+			wp_register_style( 'bootstrap-4-shortcodes', BS4_SHORTCODES_URL . '/dist/css/main.css' );
+			wp_enqueue_style( 'bootstrap-4-shortcodes' );
+			//Visual Composer is nothing but problems
+			$handle = 'vc_bootstrap_js';
+			$list = 'enqueued';
+			if (wp_script_is( $handle, $list )) {
+					wp_dequeue_script( $handle );
+			}
+	}
 
 
 	/**
--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,61 @@
 var gulp = require('gulp');
 var markdown = require('gulp-markdown');
+var sass		= require('gulp-sass');
+var mainBowerFiles	= require('main-bower-files');
+var concat		= require('gulp-concat');
+var runSequence		= require('run-sequence');
 
-gulp.task('default', function () {
+var config = {
+ 	dist: './dist',
+ 	assets: './assets' 
+}
+
+
+// ### Clean
+// `gulp clean` - Deletes the build folder entirely.
+gulp.task('clean', require('del').bind(null, ['./dist']));
+
+
+// ### Styles
+// `gulp styles` - Compiles, combines, and optimizes Bower CSS and project CSS.
+gulp.task('styles', function() { 
+	return gulp.src(config.assets + '/styles/main.scss')
+		.pipe(sass())
+		.pipe(gulp.dest(config.dist + '/css'));
+});
+
+
+// Scripts
+gulp.task("scripts", function(){
+	return gulp.src(config.assets + '/scripts/*.js')
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest(config.dist + '/js'))
+});
+
+// ### Fonts
+gulp.task('fonts', function() {
+	return gulp.src(config.assets + '/fonts/**.*')
+		.pipe(gulp.dest(config.dist + '/fonts'));
+});
+
+gulp.task('docs', function () {
 		return gulp.src('README.md')
 				.pipe(markdown())
 				.pipe(gulp.dest('docs'));
+});
+
+//Build task
+gulp.task('build', function(callback) {
+	runSequence('clean',
+			'styles',
+			'scripts',
+			'fonts',
+			'docs',
+			callback);
+});
+
+// ### Gulp
+// `gulp` - Run a complete build. To compile for production run `gulp --production`.
+gulp.task('default', function() {
+	gulp.start('build');
 });
