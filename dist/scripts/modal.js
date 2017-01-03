8fd8153bcb758ede82919f7f29cbdb085512246f
mwdelaney <michael.delaney@gmail.com>
2017-01-03 17:10:24 -0500

    Better line-breaks for inserted examples

--- dist/scripts/modal.js
+++ dist/scripts/modal.js
@@ -1,12 +1,14 @@
 jQuery(document).ready(function() {
 
 	jQuery("#bootstrap-4-shortcodes-help .insert-code").click(function() {
-		console.log('firing');
 			var path = jQuery( this ).data('path');
 			var example = jQuery( this )
 				.closest('.card')
 				.find("code")
-				.text().replace('placeholder-path', path);
+				.text().replace('placeholder-path', path)
+				.replace(/]\n/g, ']<br>\n')
+				.replace(/\.\.\.\n/g, '...<br>\n');
+			console.log(example);
 			var lines = example.split('\n');
 			var paras = '';
 			jQuery.each(lines, function(i, line) {
